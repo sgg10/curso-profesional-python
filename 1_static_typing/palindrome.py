5bd2ffe525deba47ede97a444a39529ebc770d54
root <root@DESKTOP-173PQ1T.localdomain>
2021-07-30 09:14:41 -0500

    Add pre-commit and rules

--- 1_static_typing/palindrome.py
+++ 1_static_typing/palindrome.py
@@ -1,15 +1,18 @@
 from typing import List
 
+
 def is_palindrome(string: str) -> bool:
-  string = string.replace(" ", "").lower()
-  return string == string[::-1]
+    string = string.replace(" ", "").lower()
+    return string == string[::-1]
+
 
 def run():
-  words: List[str]  = ["Oso", "Hannah", "Sebastian", 1000]
-  for word in words:
-    print(f"Is {word} a palindrome? {is_palindrome(word)}")
-  is_palindrome(1000000)
+    words: List[str] = ["Oso", "Hannah", "Sebastian"]
+    for word in words:
+        print(f"Is {word} a palindrome? {is_palindrome(word)}")
+    # is_palindrome(1000000)
+
 
 if __name__ == "__main__":
-  # mypy 1_static_typing/palindrome.py --check-untyped-defs
-  run()
+    # mypy 1_static_typing/palindrome.py --check-untyped-defs
+    run()
--- 1_static_typing/prime.py
+++ 1_static_typing/prime.py
@@ -1,14 +1,17 @@
 from typing import List
 
+
 def is_prime(n: int) -> bool:
-  return False if n < 2 else len([i for i in range(2, n) if n % i == 0]) == 0
+    return False if n < 2 else len([i for i in range(2, n) if n % i == 0]) == 0
+
 
 def run():
-  n_list: List[int] = [i for i in range(10)]
-  for n in n_list:
-    print(f"Is {n} a prime number? {is_prime(n)}")
-  is_prime("Pepe")
+    n_list: List[int] = [i for i in range(10)]
+    for n in n_list:
+        print(f"Is {n} a prime number? {is_prime(n)}")
+    # is_prime("Pepe")
 
-if __name__ == '__main__':
-  # mypy 1_static_typing/prime.py --check-untyped-defs
-  run()
+
+if __name__ == "__main__":
+    # mypy 1_static_typing/prime.py --check-untyped-defs
+    run()
